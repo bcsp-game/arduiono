ffaef1cbb8dcb0b2f6a81a245b7013ef3b3a4f47
Denys Potapov <denys.potapov@gmail.com>
2020-01-31 22:27:30 +0200

    add second reader

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SoftwareSerial.h>
 
 #define DRIVER_L_IN1 A0
 #define DRIVER_L_IN2 A1
@@ -9,11 +10,17 @@
 #define DRIVER_R_IN4 A3
 #define DRIVER_R_ENB 6
 
-#define RFID_B_SS 8
-#define RFID_T_SS 10
+#define RFID_T_SS 8
+#define RFID_B_SS 10
 #define RFID_RST 9
 
-MFRC522 rfid_top(RFID_T_SS, RFID_RST);  // Create MFRC522 instance
+#define BT_RX 2
+#define BT_TX 3
+
+MFRC522 rfid_top(RFID_T_SS, RFID_RST);     // Create MFRC522 instance
+MFRC522 rfid_bottom(RFID_B_SS, RFID_RST);  // Create MFRC522 instance
+
+SoftwareSerial bluetooth(BT_TX, BT_RX);
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,14 +35,19 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
 
-  rfid_top.PCD_Init();
+  rfid_top.PCD_Init(RFID_T_SS, RFID_RST);
   rfid_top.PCD_DumpVersionToSerial();
+
+  rfid_bottom.PCD_Init(RFID_B_SS, RFID_RST);
+  rfid_bottom.PCD_DumpVersionToSerial();
+
+  bluetooth.begin(9600);
   Serial.println(F("Done setup..."));
 }
 
 void dump_serial(MFRC522 rfid){
-  if ((!rfid_top.PICC_IsNewCardPresent()) || (!rfid_top.PICC_ReadCardSerial())) {
-    Serial.println(F("No card"));
+  if ((!rfid.PICC_IsNewCardPresent()) || (!rfid.PICC_ReadCardSerial())) {
+    // Serial.println(F("No card"));
     return ;
   }
 
@@ -55,11 +67,24 @@ void loop() {
   digitalWrite(DRIVER_L_IN1, LOW);
   digitalWrite(DRIVER_L_IN2, HIGH);
 
-  analogWrite(DRIVER_L_ENA, 64);
-  dump_serial(rfid_top);
-  delay(1000);
-
+  digitalWrite(DRIVER_R_IN3, HIGH);
+  digitalWrite(DRIVER_R_IN4, LOW);
+  
   analogWrite(DRIVER_L_ENA, 255);
+  analogWrite(DRIVER_R_ENB, 255);
+
   dump_serial(rfid_top);
-  delay(1000);
+  dump_serial(rfid_bottom);
+
+  // Если пришли данные по Bluetooth
+  while (bluetooth.available())
+  {
+    // Записываем, что пришло от Bluetooth в переменную bt_input
+    int bt_input = (int) bluetooth.read();
+    analogWrite(DRIVER_L_ENA, (bt_input - 49) * 64);
+    dump_serial(rfid_top);
+  
+    Serial.println(bt_input);
+    Serial.println((char) bt_input);
+  }
 }
