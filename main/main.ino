0b2e2bfd6f2a3f89c7154f2496e369e356b2cf9c
Denys Potapov <denys.potapov@gmail.com>
2020-02-01 02:51:35 +0200

    fix driver orientation

--- main/main.ino
+++ main/main.ino
@@ -2,13 +2,13 @@
 #include <MFRC522.h>
 #include <SoftwareSerial.h>
 
-#define DRIVER_L_IN1 A0
-#define DRIVER_L_IN2 A1
-#define DRIVER_L_ENA 5
+#define DRIVER_R_IN1 A0
+#define DRIVER_R_IN2 A1
+#define DRIVER_R_ENA 5
 
-#define DRIVER_R_IN3 A2
-#define DRIVER_R_IN4 A3
-#define DRIVER_R_ENB 6
+#define DRIVER_L_IN3 A2
+#define DRIVER_L_IN4 A3
+#define DRIVER_L_ENB 6
 
 #define RFID_T_SS 8
 #define RFID_B_SS 10
@@ -22,15 +22,18 @@ MFRC522 rfid_bottom(RFID_B_SS, RFID_RST);  // Create MFRC522 instance
 
 SoftwareSerial bluetooth(BT_TX, BT_RX);
 
+int driver_left[3] = {DRIVER_R_IN2, DRIVER_R_IN1, DRIVER_R_ENA};
+int driver_right[3] = {DRIVER_L_IN3, DRIVER_L_IN4, DRIVER_L_ENB};
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(DRIVER_L_IN1, OUTPUT);
-  pinMode(DRIVER_L_IN2, OUTPUT);
-  pinMode(DRIVER_L_ENA, OUTPUT);
+  pinMode(driver_left[0], OUTPUT);
+  pinMode(driver_left[1], OUTPUT);
+  pinMode(driver_left[2], OUTPUT);
 
-  pinMode(DRIVER_R_IN3, OUTPUT);
-  pinMode(DRIVER_R_IN4, OUTPUT);
-  pinMode(DRIVER_R_ENB, OUTPUT);
+  pinMode(driver_right[0], OUTPUT);
+  pinMode(driver_right[1], OUTPUT);
+  pinMode(driver_right[2], OUTPUT);
 
   Serial.begin(9600);
   SPI.begin();
@@ -73,6 +76,20 @@ void dump_serial(MFRC522 rfid){
   rfid.PCD_StopCrypto1();
 }
 
+void set_speed(int driver[3], int speed) {
+  if (speed == 0) {
+    digitalWrite(driver[0], LOW);
+    digitalWrite(driver[1], LOW);
+
+    return ;
+  }
+
+  digitalWrite(driver[0], (speed > 0 ? HIGH : LOW));
+  digitalWrite(driver[1], (speed > 0 ? LOW : HIGH));
+
+  analogWrite(driver[2], abs(speed));
+}
+
 void loop() {
   Serial.println(F("Waiting for card..."));
   while (!rfid_top.PICC_IsNewCardPresent()) {
@@ -85,24 +102,18 @@ void loop() {
  
   Serial.println(F("GOOOOOO"));
 
-  digitalWrite(DRIVER_L_IN1, LOW);
-  digitalWrite(DRIVER_L_IN2, HIGH);
-
-  digitalWrite(DRIVER_R_IN3, HIGH);
-  digitalWrite(DRIVER_R_IN4, LOW);
-  
-  analogWrite(DRIVER_L_ENA, 255);
-  analogWrite(DRIVER_R_ENB, 255);
+  set_speed(driver_left, 128);
+  set_speed(driver_right, -255);
   
   while (card_present(rfid_top)) {
+    dump_serial(rfid_bottom);
     while (bluetooth.available())
     {
       int bt_input = (int) bluetooth.read();
-      Serial.println((char) bt_input);
+      Serial.print((char) bt_input);
     }
   }
   Serial.println(F("Died ...")); 
-  analogWrite(DRIVER_L_ENA, 0);
-  analogWrite(DRIVER_R_ENB, 0);
-  
+  set_speed(driver_left, 0);
+  set_speed(driver_right, 0);
 }
