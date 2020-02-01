bcc908cb0011f05cc6145b33cc80ab7b9511adb4
chuq <oleksandr.lapchuk@gmail.com>
2020-02-02 01:12:39 +0200

    Update main.ino
    new proto. fixed direction

--- main/main.ino
+++ main/main.ino
@@ -17,13 +17,22 @@
 #define BT_RX 2
 #define BT_TX 3
 
+#define SPEED_ZERO 64
+#define SPEED_RATIO 5
+
+enum MotorSide
+{
+  LEFT_MOTOR,
+  RIGHT_MOTOR
+};
+
 MFRC522 rfid_top(RFID_T_SS, RFID_RST);     // Create MFRC522 instance
 MFRC522 rfid_bottom(RFID_B_SS, RFID_RST);  // Create MFRC522 instance
 
 SoftwareSerial bluetooth(BT_TX, BT_RX);
 
-int driver_left[3] = {DRIVER_R_IN2, DRIVER_R_IN1, DRIVER_R_ENA};
-int driver_right[3] = {DRIVER_L_IN3, DRIVER_L_IN4, DRIVER_L_ENB};
+const int driver_left[3] = {DRIVER_L_IN4, DRIVER_L_IN3, DRIVER_L_ENB};
+const int driver_right[3] = {DRIVER_R_IN1, DRIVER_R_IN2, DRIVER_R_ENA};
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,7 +61,7 @@ bool card_present(MFRC522 rfid) {
   byte bufferATQA[2];
   byte bufferSize = sizeof(bufferATQA);
   rfid.PCD_WriteRegister(0x24, 0x26);
-  int status = rfid.PICC_RequestA(bufferATQA, bufferSize);
+  int status = rfid.PICC_RequestA(bufferATQA, &bufferSize);
   
   bool result = rfid.PICC_IsNewCardPresent();
   return result;
@@ -65,8 +74,9 @@ void dump_serial(MFRC522 rfid){
     return ;
   }
 
-  log("collected ");
+  log("collected");
   log(String(rfid.uid.uidByte[0], HEX).c_str());
+  bluetooth.print('C');
   
 //  for (byte i = 0; i < rfid.uid.size; i++) {
 //    log(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -79,7 +89,11 @@ void dump_serial(MFRC522 rfid){
   rfid.PCD_StopCrypto1();
 }
 
-void set_speed(int driver[3], int speed) {
+void set_speed(MotorSide side, int speed)
+{
+  
+  const int* driver = (side == LEFT_MOTOR) ? driver_left : driver_right;
+  
   if (speed == 0) {
     digitalWrite(driver[0], LOW);
     digitalWrite(driver[1], LOW);
@@ -90,52 +104,65 @@ void set_speed(int driver[3], int speed) {
   digitalWrite(driver[0], (speed > 0 ? HIGH : LOW));
   digitalWrite(driver[1], (speed > 0 ? LOW : HIGH));
 
-  analogWrite(driver[2], min(255, abs(speed)));
+  analogWrite(driver[2], min(255, abs(speed) + 30));
 }
 
-void log(char* s) {
+void set_speed(int speed)
+{
+  set_speed(LEFT_MOTOR, speed);
+  set_speed(RIGHT_MOTOR, speed);
+}
+
+void log(const char* s)
+{
   Serial.println(s);
-  bluetooth.print(s); 
 }
 
-void loop() {
-  log("Waiting for card...");
-  while (!rfid_top.PICC_IsNewCardPresent()) {
-    delay(100);
-     // Halt PICC
-    rfid_top.PICC_HaltA();
-    // Stop encryption on PCD
-    rfid_top.PCD_StopCrypto1();
+void loop()
+{
+ log("Waiting for card...");
+ bluetooth.print('W');
 
-    // clear bluetooth
-    while (bluetooth.available()) {
-      bluetooth.read();
-    };
-  };
+ while (!rfid_top.PICC_IsNewCardPresent()) {
+   delay(100);
+    // Halt PICC
+   rfid_top.PICC_HaltA();
+   // Stop encryption on PCD
+   rfid_top.PCD_StopCrypto1();
+
+   // clear bluetooth
+   while (bluetooth.available()) {
+     bluetooth.read();
+   };
+ };
 
   log("GOOOOOO");
+  bluetooth.print('G');
 
-  set_speed(driver_left, 0);
-  set_speed(driver_right, 0);
+  set_speed(0);
   
-  while (card_present(rfid_top)) {
-    dump_serial(rfid_bottom);
-    if (bluetooth.available() >= 2)
-    {
-      int right = (int) bluetooth.read();
-      int left = (int) bluetooth.read();
-      
-      set_speed(driver_left, (left - 128) * 3);
-      set_speed(driver_right, (right - 128) * 3);
+ while (card_present(rfid_top))
+ {
+   dump_serial(rfid_bottom);
+   
+   if (bluetooth.available())
+   {
+     const byte value = bluetooth.read();
+     
+     const int speed = SPEED_RATIO * ( (value & 127) - SPEED_ZERO );
+     const MotorSide side = (value & 128) ? RIGHT_MOTOR : LEFT_MOTOR;
+     
+     set_speed(side, speed);
 
-      Serial.print(left, DEC);
-      Serial.print(" "); 
-      Serial.print(right, DEC);
-      Serial.println(); 
-    }
-  }
-  log("Died");
-  bluetooth.print(F("Died ...")); 
-  set_speed(driver_left, 0);
-  set_speed(driver_right, 0);
+    //  Serial.print(side, DEC);
+    //  Serial.print(" ");
+    //  Serial.print(speed, DEC);
+    //  Serial.println();
+   }
+ }
+  
+//  log("Died");
+//  bluetooth.print(F("Died ..."));
+ 
+ set_speed(0);
 }
